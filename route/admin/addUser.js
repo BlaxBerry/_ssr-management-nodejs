e19d103fca3690b44bd3267ce483b3cad6c1a8ad
Chen <chenjiaxu333@gmail.com>
2021-07-11 18:11:58 +0900

    add new user

--- route/admin/addUser.js
+++ route/admin/addUser.js
@@ -1,5 +1,9 @@
 // Joi
 const Joi = require('joi')
+// bcrypt
+const bcrypt = require('bcrypt')
+// 用户集合
+const { User } = require('../../model/user')
 
 // add user
 module.exports = async (req, res) => {
@@ -18,7 +22,7 @@ module.exports = async (req, res) => {
             .required()
             .error(new Error('Password does not conform to the format specification')),
         identity: Joi.string()
-            .valid('normal', 'admin')
+            .valid('Normal', 'Admin')
             .required()
             .error(new Error('Identity does not conform to the format specification')),
         status: Joi.number()
@@ -32,6 +36,25 @@ module.exports = async (req, res) => {
         await Joi.validate(req.body, schema)
     } catch (err) {
         // if failed
-        res.redirect(`/admin/add/user?message=${err.message}`)
+        return res.redirect(`/admin/add/user?message=${err.message}`)
     }
+
+    // check if there is this user in MongoDB
+    const user = await User.findOne({
+        email: req.body.email
+    })
+    if (user) {
+        // 邮箱已被注册
+        return res.redirect('/admin/add/user?message=Email is already be used')
+    } else {
+        // 加密密码
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt)
+    }
+
+    // add to MongoDB' blog
+    await User.create(req.body)
+
+    // redirect
+    res.redirect('/admin/users')
 }
