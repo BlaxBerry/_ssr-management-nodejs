51af0862d87718f871320b4df3ccc88be26825bd
Chen <chenjiaxu333@gmail.com>
2021-07-11 14:53:08 +0900

    Route interception /Login interception

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 const express = require('express')
 const path = require('path')
+// body-paser
 const bodyParser = require('body-parser')
+// MongoDB
+require('./model/connect')
+// Router
+const home = require('./route/home')
+const admin = require('./route/admin')
+// session
+const session = require('express-session')
 
 // create HTTP server
 const app = express()
-
-// session
-const session = require('express-session')
 
 // bodyParser 
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // session
-app.use(session({secret:'secret key'}))
-
-
-// MongoDB
-require('./model/connect')
+app.use(session({ secret: 'secret key' }))
 
 // static 
 app.use(express.static(path.join(__dirname, 'public')))
 
+// 登陆拦截
+app.use('/admin', (req, res, next) => {
+    // 判断地址是否是login页面 && session对象中是否存有自定义属性（即是否登陆）
+    if (req.url != '/login' && !req.session.username) {
+        res.redirect('/admin/login')
+    } else {
+        next();
+    }
+})
+
 // Router
-const home = require('./route/home')
-const admin = require('./route/admin')
 app.use('/home', home)
 app.use('/admin', admin)
 
