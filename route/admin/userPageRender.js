1d89c56c9e8b810a16a34c29d14dc2cb2daf8f48
Chen <chenjiaxu333@gmail.com>
2021-07-11 19:07:03 +0900

    render usersList page according to MongoDB

--- route/admin/userPageRender.js
+++ route/admin/userPageRender.js
@@ -1,5 +1,12 @@
+// 用户集合
+const { User } = require('../../model/user')
 // render user list page
 // /admin/users
-module.exports = (req, res) => {
-    res.render('admin/users')
+module.exports = async (req, res) => {
+    // get all list from MongoDB
+    let users = await User.find({})
+    // render list in template
+    res.render('admin/users', {
+        users: users
+    })
 }
