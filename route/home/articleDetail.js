e9f978d4c6f31677d30e80dfd7541d7763eb5a2f
Chen <chenjiaxu333@gmail.com>
2021-07-13 14:36:00 +0900

    client home article detail page

--- route/home/articleDetail.js
+++ route/home/articleDetail.js
@@ -1,4 +1,16 @@
+const { Article } = require('../../model/article')
+
 // home article detail  /home/article
-module.exports = (req, res) => {
-    res.render('home/articleDetail')
+module.exports = async (req, res) => {
+    const id = req.query.id;
+
+    // find data in MongoDB
+    const article = await Article.findOne({
+        _id: id
+    }).populate('author').lean()
+
+    // render
+    res.render('home/articleDetail', {
+        article: article
+    })
 }
